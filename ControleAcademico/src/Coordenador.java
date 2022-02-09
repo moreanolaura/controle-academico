a70564336e17df65d8b5c001bfb5a2f5c01a1f6f
Vando Diniz <vandocarlos2001@outlook.com>
2022-02-08 21:38:05 -0300

    Create Coordenador.java

---
+++ ControleAcademico/src/Coordenador.java
@@ -0,0 +1,30 @@
+public class Coordenador extends Usuario{
+
+    public Coordenador(String nomeCompleto, String login, String senha, String email) {
+        super(nomeCompleto, login, senha, email);
+    }
+
+    private void criar_disciplina(){
+
+    }
+
+    private void criar_turma(){
+
+    }
+
+    private void matriculaAlunos(){
+
+    }
+
+    private void editarTurmas(){
+
+    }
+
+    private void deleta_turma(){
+
+    }
+
+    private void excluir_disciplina(){
+
+    }
+}
